7d6b63fab7c961e1c421619a13be1239fa28df01
caio-muniz <caaioomunizz@gmail.com>
2026-09-02 12:14:23 -0300

    feat: modify agent initialization and update logic

--- src/main.py
+++ src/main.py
@@ -5,8 +5,8 @@ pygame.init()
 screen = pygame.display.set_mode((1000, 700))
 running = True
 
-agent1 = Agent(400, 300, 0.4, 0, (255, 255, 0), 50)
-agent2 = Agent(200, 100, 0.4, 1,(255, 255, 255), 10)
+agent1 = Agent(400, 300, 1, 1, (255, 255, 0), 50)
+agent2 = Agent(200, 100, 1, 3,(255, 255, 255), 10)
 
 
 while running:
@@ -14,12 +14,17 @@ while running:
         if event.type == pygame.QUIT:
             running = False
 
+    agent1.check_bounds(1000,700)
+    agent2.check_bounds(1000,700)
 
-    screen.fill((0,0,0))
-    agent1.draw(screen)
-    agent2.draw(screen)
     agent1.update()
     agent2.update()
+    
+
+    screen.fill((0,0,0))
+
+    agent1.draw(screen)
+    agent2.draw(screen)
 
     pygame.display.flip()
 
