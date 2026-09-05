9760eac916bbf4f8839918ff8e50464af884feb7
caio-muniz <caaioomunizz@gmail.com>
2026-09-05 12:30:50 -0300

    feat: refactor Agent class methods and add speed attribute

--- src/agent.py
+++ src/agent.py
@@ -1,33 +1,46 @@
 import pygame
 import math
 
+
 class Agent:
     def __init__(self, x, y, vx, vy, color, size):
         self.x = x
         self.y = y
+
         self.vx = vx
         self.vy = vy
+
         self.color = color
         self.size = size
 
+        self.speed = 2
+
     def draw(self, screen):
-        pygame.draw.circle(screen, self.color, (self.x, self.y), self.size)
+        pygame.draw.circle(
+            screen,
+            self.color,
+            (int(self.x), int(self.y)),
+            self.size
+        )
 
-    def update(self):
-        self.x = self.x + self.vx
-        self.y = self.y + self.vy
+    def update(self, border_x, border_y):
+        self.x += self.vx
+        self.y += self.vy
 
-    def check_bounds(self, borderX , borderY):
-        if self.x >= borderX - self.size:
-            self.x = borderX - self.size
+        self.check_bounds(border_x, border_y)
+
+    def check_bounds(self, border_x, border_y):
+
+        if self.x >= border_x - self.size:
+            self.x = border_x - self.size
             self.vx = -abs(self.vx)
 
         if self.x <= self.size:
             self.x = self.size
             self.vx = abs(self.vx)
 
-        if self.y >= borderY - self.size:
-            self.y = borderY - self.size
+        if self.y >= border_y - self.size:
+            self.y = border_y - self.size
             self.vy = -abs(self.vy)
 
         if self.y <= self.size:
@@ -40,17 +53,34 @@ class Agent:
 
         return math.sqrt(dx**2 + dy**2)
 
-    def move_towards(self, alvo):
+    def get_state(self, alvo):
         dx = alvo.x - self.x
         dy = alvo.y - self.y
 
-        distance = math.sqrt(dx**2 + dy**2)
+        return [dx, dy]
 
-        dx /= distance
-        dy /= distance
+    def apply_action(self, action):
 
-        self.vx = dx * 2
-        self.vy = dy * 2
+        if action == 0:
+            self.vx = 0
+            self.vy = self.speed
 
+        elif action == 1:
+            self.vx = 0
+            self.vy = -self.speed
 
+        elif action == 2:
+            self.vx = self.speed
+            self.vy = 0
 
+        elif action == 3:
+            self.vx = -self.speed
+            self.vy = 0
+
+    def calculate_reward(self, alvo, distancia_anterior):
+        distancia_atual = self.distance_to(alvo)
+
+        if distancia_atual < distancia_anterior:
+            return 1
+        else:
+            return -1
